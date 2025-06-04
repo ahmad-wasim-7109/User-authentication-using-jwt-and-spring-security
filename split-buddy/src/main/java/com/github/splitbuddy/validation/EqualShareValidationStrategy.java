b7fd80cee94da302fefc8a23c0d65f1e751c0a02
Md Wasim Ahmad <wasimahmad15042000@gmail.com>
2025-06-05 02:22:43 +0530

    updated logic for equal split strategy

--- split-buddy/src/main/java/com/github/splitbuddy/validation/EqualShareValidationStrategy.java
+++ split-buddy/src/main/java/com/github/splitbuddy/validation/EqualShareValidationStrategy.java
@@ -6,6 +6,14 @@ import com.github.splitbuddy.exception.InvalidDataException;
 public class EqualShareValidationStrategy implements ExpenseValidationStrategy {
     @Override
     public void validate(ExpenseCreationRequest request) throws InvalidDataException {
+        double totalAmount = request.getShares().stream()
+                .mapToDouble(s -> s.amountOwed() == null ? 0 : s.amountOwed())
+                .sum();
+
+        if (Math.abs(totalAmount - request.getTotalAmount()) > 0.01) {
+            throw new InvalidDataException("Total amount must be equal to sum of shares");
+        }
+
         double equalShare = request.getTotalAmount() / request.getShares().size();
         for (final var share : request.getShares()) {
             if (Math.abs(share.amountOwed() - equalShare) > 0.01) {
